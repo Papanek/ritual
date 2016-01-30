c25adbd7fbae3a5a9383dc5884244eeb8179e017
Daniel Papanek <papanek82@gmail.com>
2016-01-29 18:46:46 -0800

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,12 @@
+/**
+ * ******************************
+ * Project: ritual
+ * Creator: Daniel Papanek
+ * Date :   1/29/2016
+ * ******************************
+ **/
+public class main {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
