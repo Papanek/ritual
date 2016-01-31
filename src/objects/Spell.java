5cfa6c1563b6114a95967c893ae05132857d1c00
brewiebe <brewiebe@gmail.com>
2016-01-30 18:06:33 -0800

    added player and enemy images

--- src/objects/Spell.java
+++ src/objects/Spell.java
@@ -2,9 +2,14 @@ package objects;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 import interfaces.Drawable;
 import interfaces.Movable;
+
+import javax.imageio.ImageIO;
 
 /**
  * ******************************
@@ -21,20 +26,26 @@ public class Spell implements Drawable, Movable{
     protected float speedY;
     protected int x, y;
     protected Color spellColor;
-    
+	File img = new File("resource/fireballbigger.png");
+	BufferedImage spellImage;
+
     public Spell(int x, int y, float speedX, float speedY){
     	this.x = x ; // adjust this to get spell to shoot from staff
     	this.y = y + 50; // and this
     	this.speedX = speedX;
     	this.speedY = speedY;
     	this.spellColor = Color.RED;
+		try{
+			spellImage= ImageIO.read(img);
+		}
+		catch(IOException e){System.out.print("fuck");}
     }
     
 	@Override
 	public void draw(Graphics2D g) {
-		g.setColor(spellColor);
+		//g.setColor(spellColor);
 		g.translate(x,  y);
-		g.drawOval(0, 0, 5, 5);
+		g.drawImage(spellImage, 0, 0, null);
 		g.translate(-x, -y);
 	}
 
